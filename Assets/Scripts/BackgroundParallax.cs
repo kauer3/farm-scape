74a0e86d180b5bfd99c23466681c608bb31acba4
Aldrone <35581934+Aldrone@users.noreply.github.com>
2023-10-26 17:09:16 +1300

    Added new background

---
+++ Assets/Scripts/BackgroundParallax.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    public List<Sprite> BackgroundSprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
