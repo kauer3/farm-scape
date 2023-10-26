57318d87a8e420a81ac30b55c2afb1702eebd7c9
Aldrone <35581934+Aldrone@users.noreply.github.com>
2023-10-26 22:34:57 +1300

    Added basic background parallax

--- Assets/Scripts/BackgroundParallax.cs
+++ Assets/Scripts/BackgroundParallax.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 
 public class BackgroundParallax : MonoBehaviour
 {
-    public List<Sprite> BackgroundSprites;
+    public List<Sprite> backgroundSprites;
+    public float parralaxX, parralaxY;
+    public GameObject cam;
+    float startPosX, startPosY, length;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosX = cam.transform.position.x;
+        startPosY = cam.transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float temp = (cam.transform.position.x * (1 - parralaxX));
+        float distX = (cam.transform.position.x * parralaxX);
+        float distY = (cam.transform.position.y * parralaxY);
+
+        transform.position = new Vector3(startPosX + distX, startPosY + distY, transform.position.z);
+
+        if (temp > startPosX + length) startPosX += length;
     }
 }
